cfd552b533ce6761e57a2e583b401b5ef4454883
devealibarakat <Dev.ali.barakat@gmail.com>
2025-03-30 18:01:33 +0300

    change file

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,3 @@ use Orion\Facades\Orion;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::group(['as' => 'api'], function () {
-    Orion::resource('posts', PostController::class);
-});
