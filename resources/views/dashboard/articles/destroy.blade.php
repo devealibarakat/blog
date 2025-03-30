2d97f7c52ca2bede1690cda32455b1dca3817021
devealibarakat <Dev.ali.barakat@gmail.com>
2025-03-30 17:17:01 +0300

    destroy artical

---
+++ resources/views/dashboard/articles/destroy.blade.php
@@ -0,0 +1,29 @@
+<div class="modal fade" tabindex="-1" id="delete_article{{$article->id}}">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title"> {{ trans('string.delete_user') }}</h3>
+
+                <!--begin::Close-->
+                <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal"
+                    aria-label="Close">
+                    <i class="ki-duotone ki-cross fs-1"><span class="path1"></span><span class="path2"></span></i>
+                </div>
+                <!--end::Close-->
+            </div>
+
+            <div class="modal-body">
+                <form action="{{ route('articles.destroy', $article) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <h3> {{__('string.confirm_delete')}}</h3>
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">{{__('string.close')}}</button>
+                <button type="submit" class="btn btn-danger">{{__('string.delete')}}</button>
+            </div>
+        </div>
+    </div>
+</div>
