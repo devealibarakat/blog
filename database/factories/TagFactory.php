15551b7943c77ec28ef803616ce86f2dd9111c23
devealibarakat <Dev.ali.barakat@gmail.com>
2025-03-30 17:11:30 +0300

    Factories

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
+
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [];
+    }
+}
