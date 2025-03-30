3158b7b115b5dcfcc212f1ec01c9807b378bc8ca
devealibarakat <Dev.ali.barakat@gmail.com>
2025-03-30 17:58:11 +0300

    change file

--- resources/views/dashboard/tags/create.blade.php
+++ resources/views/dashboard/tags/create.blade.php
@@ -27,17 +27,12 @@
                 <div class="tab-content mt-3 row">
                     @foreach (LaravelLocalization::getSupportedLocales() as $locale => $properties)
                         <div class="tab-pane fade show {{ $loop->first ? 'active' : '' }}" id="{{ $locale }}">
-                            <div class="m-3 col-lg-10 ">
-
-                                <label for="title_{{ $locale }}" class="form-label">{{ __('inputs.title.label') }}
+                            <div class="m-5">
+                                <label for="name{{ $locale }}" class="form-label">{{ __('inputs.title.label') }}
                                     ({{ $properties['native'] }})</label>
-
                                 <input type="text" class="form-control" name="name[{{ $locale }}]" id="name{{ $locale }}"
-                                    value="{{ $tag->name }}"
                                     placeholder="{{__('inputs.title.placeholder') }} ({{ $properties['native'] }})">
-
                                 @error("name.$locale") <span class="text-danger">{{ $message }}</span> @enderror
-
                             </div>
                         </div>
                     @endforeach
